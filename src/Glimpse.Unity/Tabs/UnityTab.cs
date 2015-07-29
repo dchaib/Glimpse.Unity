48e56863eea3bd48de8cfed2ef23b472e198a812
Serge Populov <populov@gmail.com>
2015-07-29 21:52:01 +0500

    Structured view

--- src/Glimpse.Unity/Tabs/UnityTab.cs
+++ src/Glimpse.Unity/Tabs/UnityTab.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Glimpse.Core.Extensibility;
 using Microsoft.Practices.Unity;
 
@@ -18,23 +20,70 @@ namespace Glimpse.Unity.Tabs
             return GetUnityContainerData(RegisteredContainer);
         }
 
-        private object GetUnityContainerData(IUnityContainer container)
+        private static object GetUnityContainerData(IUnityContainer container)
         {
             if (container == null)
             {
                 return null;
             }
 
-            var registrations = container.Registrations.Select(x => new
-            {
-                Name = x.Name,
-                RegisteredType = x.RegisteredType,
-                MappedToType = x.MappedToType,
-                LifetimeManagerType = x.LifetimeManagerType,
-                LifetimeManager = x.LifetimeManager
-            });
+            var registrations = container.Registrations.ToArray();
 
-            return registrations;
+            return new UnityTabModel
+            {
+                Nameless = registrations.Where(r => string.IsNullOrEmpty(r.Name)).Select(r => new NamelessRegistration(r)),
+                Named = registrations.Where(r => !string.IsNullOrEmpty(r.Name))
+                    .GroupBy(r => r.RegisteredType)
+                    .Select(g => new NamedRegistrationList(g))
+            };
+        }
+
+        public bool KeysHeadings { get { return true; } }
+
+        internal class UnityTabModel
+        {
+            public IEnumerable<NamelessRegistration> Nameless { get; set; }
+            public IEnumerable<NamedRegistrationList> Named { get; set; }
+        }
+
+        internal class NamelessRegistration
+        {
+            public NamelessRegistration(ContainerRegistration r)
+            {
+                RegisteredType = r.RegisteredType;
+                MappedToType = r.MappedToType;
+                LifeTimeManagerType = r.LifetimeManagerType;
+            }
+
+            public Type RegisteredType { get; set; }
+            public Type MappedToType { get; set; }
+            public Type LifeTimeManagerType { get; set; }
+        }
+
+        internal class NamedRegistrationList
+        {
+            public NamedRegistrationList(IGrouping<Type, ContainerRegistration> registrations)
+            {
+                RegisteredType = registrations.Key;
+                Registrations = registrations.Select(r => new NamedRegistration(r));
+            }
+
+            public Type RegisteredType { get; set; }
+            public IEnumerable<NamedRegistration> Registrations { get; set; }
+        }
+
+        internal class NamedRegistration
+        {
+            public NamedRegistration(ContainerRegistration r)
+            {
+                Name = r.Name;
+                MappedToType = r.MappedToType;
+                LifeTimeManagerType = r.LifetimeManagerType;
+            }
+
+            public string Name { get; set; }
+            public Type MappedToType { get; set; }
+            public Type LifeTimeManagerType { get; set; }
         }
     }
 }
